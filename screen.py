ca6d8457d44dea80b51d123b727d74cc23c4bcf6
Francesco Pessolano <fpessolano@users.noreply.github.com>
2023-07-12 21:06:10 +0200

    .

---
+++ screen.py
@@ -0,0 +1,64 @@
+import curses
+
+class CommandType:
+    NONE = 0
+    END = 1
+    MOVE = 2
+
+class Command:
+    def __init__(self):
+        self.cmd = CommandType.NONE
+        self.data = []
+
+class Screen:
+    def __init__(self, rows, columns):
+        self.matrix = [[' ' for _ in range(columns)] for _ in range(rows)]
+        self.playHeight = rows
+        self.playWidth = columns
+
+        self.stdscr = curses.initscr()
+        curses.cbreak()
+        curses.noecho()
+        curses.curs_set(0)
+        self.stdscr.keypad(True)
+        self.stdscr.nodelay(True)
+        self.screenHeight, self.screenWidth = self.stdscr.getmaxyx()
+
+    def write(self, row, column, value):
+        self.stdscr.addch(row, column, value)
+        self.matrix[row][column] = value
+
+    def read(self, row, column):
+        return self.matrix[row][column]
+
+    def getCommand(self, tries=3):
+        cmd = Command()
+        cmd.cmd = CommandType.NONE
+
+        for _ in range(tries):
+            ch = self.stdscr.getch()
+            if ch == curses.KEY_UP:
+                cmd.cmd = CommandType.MOVE
+                cmd.data = [0, -1]
+                break
+            elif ch == curses.KEY_DOWN:
+                cmd.cmd = CommandType.MOVE
+                cmd.data = [0, 1]
+                break
+            elif ch == curses.KEY_LEFT:
+                cmd.cmd = CommandType.MOVE
+                cmd.data = [-1, 0]
+                break
+            elif ch == curses.KEY_RIGHT:
+                cmd.cmd = CommandType.MOVE
+                cmd.data = [1, 0]
+                break
+            elif ch == 27:  # Escape key
+                cmd.cmd = CommandType.END
+                break
+
+        return cmd
+
+    def __del__(self):
+        curses.endwin()
+
---
+++ snake.py
@@ -0,0 +1,80 @@
+from dynamic_list import DynamicList
+
+class Snake:
+    def __init__(self, size: int = 5, starting_x: int = 0, starting_y: int = 0, vertical: bool = False):
+        self.minimumSize = size
+        self.growthMovements = 0
+        self.screenHeight = 0
+        self.screenWidth = 0
+        self.coords = DynamicList()
+
+        if vertical:
+            for i in range(self.minimumSize):
+                self.coords.pushFront([starting_x, starting_y + i])
+        else:
+            for i in range(self.minimumSize):
+                self.coords.pushFront([starting_x + i, starting_y])
+
+    def setPlayfield(self, width: int, height: int) -> None:
+        self.screenHeight = height
+        self.screenWidth = width
+
+    def grow(self, moves: int) -> None:
+        self.growthMovements += moves
+
+    def peek_move(self, x: int, y: int):
+        invalidMove = [-1, -1]
+        currentPosition = self.coords.getHead().copy()
+        currentPosition[0] += x
+        currentPosition[1] += y
+
+        if self.coords.getIndexInArray(currentPosition) != -1:
+            currentPosition[0] -= 2 * x
+            currentPosition[1] -= 2 * y
+
+        if (0 <= currentPosition[0] < self.screenWidth and
+                0 <= currentPosition[1] < self.screenHeight):
+            return currentPosition
+
+        return invalidMove
+
+    def move(self, x: int, y: int):
+        oldPosition = [-1, -1]
+        headPosition = self.peek_move(x, y)
+
+        if headPosition[0] != -1:
+            self.coords.pushFront(headPosition)
+
+            if self.growthMovements == 0:
+                oldPosition = self.coords.popBack()
+            else:
+                self.growthMovements -= 1
+
+        return oldPosition
+
+    def getHead(self):
+        return self.coords.getHead()
+
+    def covers(self, point):
+        return self.coords.getIndexInArray(point) != -1
+
+if __name__ == "__main__":
+    # Example usage
+    snake = Snake(size=4, starting_x=2, starting_y=2, vertical=True)
+    snake.setPlayfield(width=10, height=10)
+
+    print("Initial Head Position:", snake.getHead())
+    print("Covers (2, 3):", snake.covers([2, 3]))
+    print("Covers (5, 2):", snake.covers([5, 2]))
+
+    snake.grow(2)
+
+    print("After Growth, Head Position:", snake.getHead())
+
+    snake.move(0, 1)
+    snake.move(0, 1)
+    snake.move(1, 0)
+
+    print("After Moves, Head Position:", snake.getHead())
+    print("Covers (2, 3):", snake.covers([2, 3]))
+    print("Covers (5, 2):", snake.covers([5, 2]))
